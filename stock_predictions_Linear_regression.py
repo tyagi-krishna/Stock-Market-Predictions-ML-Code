f92ca168709b54e0eec0ede0e0d4712e3d81d2e4
tyagi-krishna <krishna2021csai133@abesit.edu.in>
2023-01-11 00:11:10 +0530

    done

--- stock_predictions_Linear_regression.py
+++ stock_predictions_Linear_regression.py
@@ -16,13 +16,13 @@ aapl_sma, aapl_met_sma = ti.get_sma(symbol='INFY')
 print('Data Collected Successfully from the Alpha Vantage API...')
 
 
-#reshaping and adjusting data into arrays to feed into the machine learning model
+# reshaping and adjusting data into arrays to feed into the machine learning model
 price = aapl_data.iloc[:, 0]
 y = np.array(price).reshape((-1,1))
 numbers = list(range(1,101))
 x = np.array(numbers).reshape((-1,1))
 
-#splitting the data into training and testing data
+# splitting the data into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2)
 SEED = 42
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = SEED)
@@ -35,7 +35,7 @@ print('Model Created Successfully...')
 print('The Intercept is:', regressor.intercept_)
 
 
-#making predictions using the model
+# making predictions using the model
 predictions = regressor.predict([[101]])
 print('The Predicted Value is:',predictions)
 ynew1 = np.append(x, [101])
